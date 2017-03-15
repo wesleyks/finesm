a695a2c915373ee8e30f6c23d79ee6be9567dfe3
Wesley Sun <wesleyksun@gmail.com>
2017-03-14 21:25:18 -0700

    increase test coverage

--- tests/test_state_machine.py
+++ tests/test_state_machine.py
@@ -9,6 +9,7 @@ class SimpleStateMachine(StateMachine):
         super(SimpleStateMachine, self).__init__()
         self.foo = False
         self.bar = False
+        self.updoot = 0
 
     @waiting.on_message('start')
     def waiting_start(self):
@@ -22,6 +23,10 @@ class SimpleStateMachine(StateMachine):
     def running_enter(self):
         self.bar = True
 
+    @running.on_update
+    def runnin_update(self):
+        self.updoot += 1
+
 
 def test_state_machine():
     state_machine = SimpleStateMachine()
@@ -30,8 +35,17 @@ def test_state_machine():
     assert state_machine.foo is False
     assert state_machine.bar is False
 
+    state_machine.update()
+
+    assert state_machine.updoot == 0
+
     state_machine.send_message('start')
 
     assert state_machine.foo is True
     assert state_machine.bar is True
     assert state_machine.state == SimpleStateMachine.running
+
+    state_machine.update()
+    state_machine.update()
+
+    assert state_machine.updoot == 2
